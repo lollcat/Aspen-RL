3338e3735fac08891fc4e355c69884639cefa231
ADChristos <c.ampatzis@student.utwente.nl>
2022-01-26 08:25:29 +0100

    Updated all the changes to the aspen_api, as well as the the updated Aspen-Simulation suite. Still need to include the economic calculations

--- hydrocarbon_problem/api/Simulation.py
+++ hydrocarbon_problem/api/Simulation.py
@@ -1,46 +1,48 @@
-import os 
+import os
 import win32com.client as win32
+
 
 class Simulation():
     AspenSimulation = win32.gencache.EnsureDispatch("Apwn.Document")
-    def __init__(self,PATH,VISIBILITY) :
+
+    def __init__(self, PATH, VISIBILITY):
         self.AspenSimulation.InitFromArchive2(os.path.abspath(PATH))
         self.AspenSimulation.Visible = VISIBILITY
-
 
     @property
     def BLK(self):
         return self.AspenSimulation.Tree.Elements("Data").Elements("Blocks")
 
-    def BLK_NumberOfStages(self,NStages):
+    def BLK_NumberOfStages(self, NStages):
         self.BLK.Elements("B1").Elements("Input").Elements("NSTAGE").Value = NStages
 
-    def BLK_FeedLocation(self,Feed_Location,Feed_Name):
+    def BLK_FeedLocation(self, Feed_Location, Feed_Name):
         self.BLK.Elements("B1").Elements("Input").Elements("FEED_STAGE").Elements(Feed_Name).Value = Feed_Location
 
-    def BLK_Pressure(self,Pressure):
+    def BLK_Pressure(self, Pressure):
         self.BLK.Elements("B1").Elements("Input").Elements("PRES1").Value = Pressure
 
-    def BLK_RefluxRatio(self,RfxR):
-        self.BLK.Elements("B1").Elements("Input").Elements("BASIS_RR").Value = RfxR 
+    def BLK_RefluxRatio(self, RfxR):
+        self.BLK.Elements("B1").Elements("Input").Elements("BASIS_RR").Value = RfxR
 
-    def BLK_ReboilerRatio(self,RblR):
-        self.BLK.Elements("B1").Elements("Input").Elements("BASIS_BR").Value = RblR   
+    def BLK_ReboilerRatio(self, RblR):
+        self.BLK.Elements("B1").Elements("Input").Elements("BASIS_BR").Value = RblR
 
     @property
     def STRM(self):
         return self.AspenSimulation.Tree.Elements("Data").Elements("Streams")
 
-    def STRM_Temperature(self,Name,Temp):
+    def STRM_Temperature(self, Name, Temp):
         self.STRM.Elements(Name).Elements("Input").Elements("TEMP").Elements("MIXED").Value = Temp
 
-    def STRM_Pressure(self,Name,Pressure):
+    def STRM_Pressure(self, Name, Pressure):
         self.STRM.Elements(Name).Elements("Input").Elements("PRES").Elements("MIXED").Value = Pressure
 
-    def STRM_Flowrate(self,Name,Chemical,Flowrate):
-        self.STRM.Elements(Name).Elements("Input").Elements("FLOW").Elements("MIXED").Elements(Chemical).Value = Flowrate
+    def STRM_Flowrate(self, Name, Chemical, Flowrate):
+        self.STRM.Elements(Name).Elements("Input").Elements("FLOW").Elements("MIXED").Elements(
+            Chemical).Value = Flowrate
 
-    def STRM_Get_Outputs(self,Name,Chemicals):
+    def STRM_Get_Outputs(self, Name, Chemicals):
         STRM_COMP = self.STRM.Elements(Name).Elements("Output").Elements("MOLEFLOW").Elements("MIXED")
         COMP_1 = STRM_COMP.Elements(Chemicals[0]).Value
         COMP_2 = STRM_COMP.Elements(Chemicals[1]).Value
@@ -48,19 +50,18 @@ class Simulation():
         COMP_4 = STRM_COMP.Elements(Chemicals[3]).Value
         COMP_5 = STRM_COMP.Elements(Chemicals[4]).Value
         COMP_6 = STRM_COMP.Elements(Chemicals[5]).Value
-        return [COMP_1, COMP_2, COMP_3,COMP_4,COMP_5,COMP_6]
-    
-    def STRM_Get_Temperature(self,Name):
+        return [COMP_1, COMP_2, COMP_3, COMP_4, COMP_5, COMP_6]
+
+    def STRM_Get_Temperature(self, Name):
         return self.STRM.Elements(Name).Elements("Input").Elements("TEMP").Elements("MIXED").Value
 
-    def STRM_Get_Pressure(self,Name):
+    def STRM_Get_Pressure(self, Name):
         return self.STRM.Elements(Name).Elements("Input").Elements("PRES").Elements("MIXED").Value
-
 
     def BLK_Get_NStages(self):
         return self.BLK.Elements("B1").Elements("Input").Elements("NSTAGE").Value
 
-    def BLK_Get_FeedLocation(self,Name):
+    def BLK_Get_FeedLocation(self, Name):
         return self.BLK.Elements("B1").Elements("Input").Elements("FEED_STAGE").Elements(Name).Value
 
     def BLK_Get_Pressure(self):
@@ -80,6 +81,39 @@ class Simulation():
 
     def BLK_Get_Column_Diameter(self):
         return self.BLK.Elements("B1").Elements("Input").Elements("CA_DIAM").Value
+
+    def BLK_Get_Column_Stage_Molar_Weights(self, N_stages):
+        M = []
+        for i in range(1, N_stages + 1):
+            M += [self.BLK.Elements("B1").Elements("Output").Elements("MW_GAS").Elements(str(i)).Value]
+        return M
+
+    def BLK_Get_Column_Stage_Temperatures(self, N_stages):
+        T = []
+        for i in range(1, N_stages + 1):
+            T += [self.BLK.Elements("B1").Elements("Output").Elements("B_TEMP").Elements(str(i)).Value]
+        return T
+
+    def BLK_Get_Column_Stage_Vapor_Flows(self, N_stages):
+        V = []
+        for i in range(1, N_stages + 1):
+            V += [self.BLK.Elements("B1").Elements("Output").Elements("VAP_FLOW").Elements(str(i)).Value]
+        return V
+
+    def BLK_Get_Column_Diameter(self, N_stages):
+        P = int(1)
+        f = float(1.6)
+        R = float(8.314)
+        Effective_Diameter = []
+        V = self.BLK_Get_Column_Stage_Vapor_Flows(N_stages)
+        M = self.BLK_Get_Column_Stage_Molar_Weights(N_stages)
+        T = self.BLK_Get_Column_Stage_Temperatures(N_stages)
+        for i in range(0, N_stages - 1):
+            Effective_Diameter += [
+                np.sqrt((4 * V[i]) / (3.1416 * f) * np.sqrt(R * (T[i] + 273.15) * M[i] * 1000 / (P * 1e5)))]
+
+        Diameter = 1.1 * max(Effective_Diameter)
+        return Diameter
 
     def Run(self):
         self.AspenSimulation.Engine.Run2()
--- hydrocarbon_problem/api/aspen_api.py
+++ hydrocarbon_problem/api/aspen_api.py
@@ -7,66 +7,90 @@ from hydrocarbon_problem.api.types import StreamSpecification, ColumnInputSpecif
 
 PATH = 'C:/Users/s2199718/Desktop/RL_PD/AspenSimulation/HydrocarbonMixture.bkp'
 
+
 class AspenAPI(BaseAspenDistillationAPI):
     def __init__(self):
-        self._flowsheet: Simulation = Simulation(PATH=PATH,VISIBILITY=False)
+        self._flowsheet: Simulation = Simulation(PATH=PATH, VISIBILITY=False)
         self._feed_name: str = "S1"
         self._tops_name: str = "S2"
         self._bottoms_name: str = "S3"
         self._name_to_aspen_name = PerCompoundProperty(ethane="ETHANE",
-        propane="PROPANE", isobutane="I-BUTANE",
-        n_butane="N-BUTANE",isopentane="I-PENTAN", n_pentane="N-PENTAN")
-        
+                                                       propane="PROPANE", isobutane="I-BUTANE",
+                                                       n_butane="N-BUTANE", isopentane="I-PENTAN", n_pentane="N-PENTAN")
 
     def set_input_stream_specification(self, stream_specification: StreamSpecification) -> None:
         """Sets the input stream to a column to fit the stream specification"""
-        #Defining the Thermodynamic Properties 
-        self._flowsheet.STRM_Temperature(self._feed_name,  stream_specification.temperature)
+        # Defining the Thermodynamic Properties
+        self._flowsheet.STRM_Temperature(self._feed_name, stream_specification.temperature)
         self._flowsheet.STRM_Pressure(self._feed_name, stream_specification.pressure)
-        #Defining the Stream Composition for the Feed
-        self._flowsheet.STRM_Flowrate(self._feed_name, self._name_to_aspen_name.ethane, stream_specification.molar_flows.ethane)
-        self._flowsheet.STRM_Flowrate(self._feed_name,self._name_to_aspen_name.propane,stream_specification.molar_flows.propane)
-        self._flowsheet.STRM_Flowrate(self._feed_name,self._name_to_aspen_name.isobutane,stream_specification.molar_flows.isobutane)
-        self._flowsheet.STRM_Flowrate(self._feed_name,self._name_to_aspen_name.n_butane,stream_specification.molar_flows.n_butane)
-        self._flowsheet.STRM_Flowrate(self._feed_name,self._name_to_aspen_name.isopentane,stream_specification.molar_flows.isopentane)
-        self._flowsheet.STRM_Flowrate(self._feed_name,self._name_to_aspen_name.n_pentane,stream_specification.molar_flows.n_pentane)
+        # Defining the Stream Composition for the Feed
+        self._flowsheet.STRM_Flowrate(self._feed_name, self._name_to_aspen_name.ethane,
+                                      stream_specification.molar_flows.ethane)
+        self._flowsheet.STRM_Flowrate(self._feed_name, self._name_to_aspen_name.propane,
+                                      stream_specification.molar_flows.propane)
+        self._flowsheet.STRM_Flowrate(self._feed_name, self._name_to_aspen_name.isobutane,
+                                      stream_specification.molar_flows.isobutane)
+        self._flowsheet.STRM_Flowrate(self._feed_name, self._name_to_aspen_name.n_butane,
+                                      stream_specification.molar_flows.n_butane)
+        self._flowsheet.STRM_Flowrate(self._feed_name, self._name_to_aspen_name.isopentane,
+                                      stream_specification.molar_flows.isopentane)
+        self._flowsheet.STRM_Flowrate(self._feed_name, self._name_to_aspen_name.n_pentane,
+                                      stream_specification.molar_flows.n_pentane)
 
     def get_output_stream_specifications(self) -> Tuple[StreamSpecification, StreamSpecification]:
+        # Getting the physical values of Top streams
+        tops_temperature = self._flowsheet.STRM_Get_Temperature(self._tops_name)
+        tops_pressure = self._flowsheet.STRM_Get_Pressure(self._tops_name)
         # Acquiring the outputs out of the Destillate (Top Stream)
-        tops_ethane = self._flowsheet.STRM_Get_Outputs(self._tops_name,self._name_to_aspen_name.ethane)
-        tops_propane = self._flowsheet.STRM_Get_Outputs(self._tops_name,self._name_to_aspen_name.propane)
-        tops_isobutane = self._flowsheet.STRM_Get_Outputs(self._tops_name,self._name_to_aspen_name.isobutane)
-        tops_n_butane = self._flowsheet.STRM_Get_Outputs(self._tops_name,self._name_to_aspen_name.n_butane)
-        tops_isopentane = self._flowsheet.STRM_Get_Outputs(self._tops_name,self._name_to_aspen_name.isopentane)
-        tops_n_pentane = self._flowsheet.STRM_Get_Outputs(self._tops_name,self._name_to_aspen_name.n_pentane)
-        # Tubulating the Results of the Destillate Stream
-        TOPS_COMPOSITION_VECTOR = [tops_ethane,tops_propane,tops_isobutane,tops_n_butane,tops_isopentane,tops_n_pentane]
-        
+        tops_ethane = self._flowsheet.STRM_Get_Outputs(self._tops_name, self._name_to_aspen_name.ethane)
+        tops_propane = self._flowsheet.STRM_Get_Outputs(self._tops_name, self._name_to_aspen_name.propane)
+        tops_isobutane = self._flowsheet.STRM_Get_Outputs(self._tops_name, self._name_to_aspen_name.isobutane)
+        tops_n_butane = self._flowsheet.STRM_Get_Outputs(self._tops_name, self._name_to_aspen_name.n_butane)
+        tops_isopentane = self._flowsheet.STRM_Get_Outputs(self._tops_name, self._name_to_aspen_name.isopentane)
+        tops_n_pentane = self._flowsheet.STRM_Get_Outputs(self._tops_name, self._name_to_aspen_name.n_pentane)
+        # Passing all the variables to their respective "slot"
+        tops_specifications = StreamSpecification(temperature=tops_temperature, pressure=tops_pressure,
+                                                  molar_flows=PerCompoundProperty(ethane=tops_ethane,
+                                                                                  propane=tops_propane,
+                                                                                  isobutane=tops_isobutane,
+                                                                                  n_butane=tops_n_butane,
+                                                                                  isopentane=tops_isopentane,
+                                                                                  n_pentane=tops_n_pentane))
+
+        # Getting the physical values of Top streams
+        bots_temperature = self._flowsheet.STRM_Get_Temperature(self._bottoms_name)
+        bots_pressure = self._flowsheet.STRM_Get_Pressure(self._bottoms_name)
         # Acquiring the outputs out of the Bottom (Bottom Stream)
-        bots_ethane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name,self._name_to_aspen_name.ethane)
-        bots_propane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name,self._name_to_aspen_name.propane)
-        bots_isobutane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name,self._name_to_aspen_name.isobutane)
-        bots_n_butane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name,self._name_to_aspen_name.n_butane)
-        bots_isopentane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name,self._name_to_aspen_name.isopentane)
-        bots_n_pentane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name,self._name_to_aspen_name.n_pentane)
-        # Tubulating the Results of the Bottom Stream 
-        BOTS_COMPOSITION_VECTOR = [bots_ethane,bots_propane,bots_isobutane,bots_n_butane,bots_isopentane,bots_n_pentane]
+        bots_ethane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name, self._name_to_aspen_name.ethane)
+        bots_propane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name, self._name_to_aspen_name.propane)
+        bots_isobutane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name, self._name_to_aspen_name.isobutane)
+        bots_n_butane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name, self._name_to_aspen_name.n_butane)
+        bots_isopentane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name, self._name_to_aspen_name.isopentane)
+        bots_n_pentane = self._flowsheet.STRM_Get_Outputs(self._bottoms_name, self._name_to_aspen_name.n_pentane)
+        # Tubulating the Results of the Bottom Stream
+        bots_specifications = StreamSpecification(temperature=bots_temperature, pressure=bots_pressure,
+                                                  molar_flows=PerCompoundProperty(ethane=bots_ethane,
+                                                                                  propane=bots_propane,
+                                                                                  isobutane=bots_isobutane,
+                                                                                  n_butane=bots_n_butane,
+                                                                                  isopentane=bots_isopentane,
+                                                                                  n_pentane=bots_n_pentane))
 
-        return TOPS_COMPOSITION_VECTOR,BOTS_COMPOSITION_VECTOR
-
+        return tops_specifications, bots_specifications
 
     def get_simulated_column_properties(self) -> ColumnOutputSpecification:
-        D_N_Stages = self._flowsheet.BLK_Get_NStages()
-        D_F_Location = self._flowsheet.BLK_Get_FeedLocation()
-        D_Pressure = self._flowsheet.BLK_Get_Pressure()
-        D_Reflux_Ratio = self._flowsheet.BLK_Get_RefluxRatio()
-        D_Reboiler_Ratio = self._flowsheet.BLK_Get_ReboilerRatio()
+        # D_F_Location = self._flowsheet.BLK_Get_FeedLocation()
+        # D_Pressure = self._flowsheet.BLK_Get_Pressure()
+        # D_Reflux_Ratio = self._flowsheet.BLK_Get_RefluxRatio()
+        # D_Reboiler_Ratio = self._flowsheet.BLK_Get_ReboilerRatio()
         D_Cond_Duty = self._flowsheet.BLK_Get_Condenser_Duty()
         D_Reb_Duty = self._flowsheet.BLK_Get_Reboiler_Duty()
         D_Col_Diameter = self._flowsheet.BLK_Get_Column_Diameter()
 
-        return [D_N_Stages,D_F_Location,D_Pressure,D_Reflux_Ratio,D_Reboiler_Ratio,D_Cond_Duty,D_Reb_Duty,D_Col_Diameter]
-        
+        D_Specifications = ColumnOutputSpecification(condensor_duty=D_Cond_Duty, reboiler_duty=D_Reb_Duty,
+                                                     diameter=D_Col_Diameter)
+
+        return D_Specifications
 
     def set_column_specification(self, column_specification: ColumnInputSpecification) -> None:
         self._flowsheet.BLK_NumberOfStages(column_specification.n_stages)
@@ -75,23 +99,19 @@ class AspenAPI(BaseAspenDistillationAPI):
         self._flowsheet.BLK_RefluxRatio(column_specification.reflux_ratio)
         self._flowsheet.BLK_ReboilerRatio(column_specification.reboil_ratio)
 
-
     def solve_flowsheet(self) -> bool:
         self._flowsheet.Run()
-
 
     def get_column_cost(self, column_specification: ColumnOutputSpecification) -> float:
         """Calculates the TAC of the column."""
         raise NotImplementedError
 
-
     def get_stream_value(self, stream_specification: StreamSpecification) -> float:
         """Calculates the value (per year) of a stream."""
         raise NotImplementedError
 
-
     def stream_is_product(self, stream_specification: StreamSpecification, product_specification:
-                                ProductSpecification) -> bool:
+    ProductSpecification) -> bool:
         """Checks whether a stream meets the product specification."""
         raise NotImplementedError
 
