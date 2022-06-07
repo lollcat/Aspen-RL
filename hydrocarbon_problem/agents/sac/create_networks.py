e2c4ee829c2e7667158a747746cda15811659527
laurence <l.midgley@instadeep.com>
2022-06-07 08:00:28 +0100

    fix: bugs in networks, networks test now passing

--- hydrocarbon_problem/agents/sac/create_networks.py
+++ hydrocarbon_problem/agents/sac/create_networks.py
@@ -19,35 +19,14 @@ def create_sac_networks(env: AspenDistillation,
                         ):
     """Create SAC networks, for now the agent always separates the given stream."""
 
-    # first let's make the policy network
     continuous_action_shape = env.action_spec()[1].shape
     assert len(continuous_action_shape) == 1 # check that actions are vector
     n_continuous_action = continuous_action_shape[0]
 
-    def policy_forward_single(observation: Observation) -> DistParams:
-        policy_net = hk.nets.MLP(policy_hidden_units + (n_continuous_action * 2),
-                                 activate_final=False)
-        policy_net_out = policy_net(observation)
-        mean, log_var = jnp.split(policy_net_out, 2, axis=-1)
-        return DistParams(mean=mean, log_var=log_var)
 
-
-    def policy_forward(observation: Union[Observation, NextObservation]) -> Union[DistParams, NextDistParams]:
-        if isinstance(observation, tuple):
-            assert len(observation) == 2
-            tops_obs, bottoms_obs = observation
-            dist_params_top = policy_forward_single(tops_obs)
-            dist_params_bot = policy_forward_single(bottoms_obs)
-            dist_params = (dist_params_top, dist_params_bot)
-        else:
-            dist_params = policy_forward_single(observation)
-        return dist_params
-
-    policy_network = hk.without_apply_rng(hk.transform(policy_forward))
-
-
-
-    # now the q-value network
+    # Define the q-value network
+    @hk.without_apply_rng
+    @hk.transform
     def q_value_network_forward_single(observation: Observation, action: Action) -> chex.Array:
         #TODO for now let's just concat the actions, later we may return 0 value if
         # choose not separate.
@@ -57,21 +36,51 @@ def create_sac_networks(env: AspenDistillation,
         q_value = q_net(q_net_in)
         return q_value
 
-    def q_network_forward(observation: Union[Observation, NextObservation],
+    def q_network_forward(q_params: chex.ArrayTree,
+                          observation: Union[Observation, NextObservation],
                           action: Union[Action, NextAction]) -> chex.Array:
         if isinstance(observation, tuple):
             assert len(observation) == 2
             assert len(action) == 2
             tops_obs, bottoms_obs = observation
             tops_action, bottoms_action = action
-            q_top = q_value_network_forward_single(tops_obs, tops_action)
-            q_bottom = q_value_network_forward_single(bottoms_obs, bottoms_action)
+            q_top = q_value_network_forward_single.apply(q_params, tops_obs, tops_action)
+            q_bottom = q_value_network_forward_single.apply(q_params, bottoms_obs, bottoms_action)
             q_value = q_top + q_bottom
         else:
-            q_value = q_value_network_forward_single(observation, action)
+            q_value = q_value_network_forward_single.apply(q_params, observation, action)
         return q_value
 
-    q_value_network = hk.without_apply_rng(hk.transform(q_network_forward))
+    q_value_network = hk.Transformed(init=q_value_network_forward_single.init,
+                                     apply=q_network_forward)
+
+    # Now let's define the policy.
+    @hk.without_apply_rng
+    @hk.transform
+    def policy_forward_single(observation: Observation) -> DistParams:
+        policy_net = hk.nets.MLP(policy_hidden_units + (n_continuous_action * 2,),
+                                 activate_final=False)
+        policy_net_out = policy_net(observation)
+        mean, log_var = jnp.split(policy_net_out, 2, axis=-1)
+        return DistParams(mean=mean, log_var=log_var)
+
+
+    def policy_forward(policy_params: chex.ArrayTree,
+                       observation: Union[Observation, NextObservation]) -> \
+            Union[DistParams, NextDistParams]:
+        if isinstance(observation, tuple):
+            assert len(observation) == 2
+            tops_obs, bottoms_obs = observation
+            dist_params_top = policy_forward_single.apply(policy_params, tops_obs)
+            dist_params_bot = policy_forward_single.apply(policy_params, bottoms_obs)
+            dist_params = (dist_params_top, dist_params_bot)
+        else:
+            dist_params = policy_forward_single.apply(policy_params, observation)
+        return dist_params
+
+    policy_network = hk.Transformed(init=policy_forward_single.init,
+                                    apply=policy_forward)
+
 
 
     def get_dist(dist_params: DistParams) -> tfp.distributions.Distribution:
@@ -84,11 +93,13 @@ def create_sac_networks(env: AspenDistillation,
     # Now let's create the log prob function
     def log_prob_single(dist_params: DistParams, action: Action) -> chex.Array:
         dist = get_dist(dist_params)
-        return dist.log_prob(action)
+        continuos_action = action[1]
+        return dist.log_prob(continuos_action)
+
 
     def log_prob(dist_params: Union[DistParams, NextDistParams],
                         action: Union[Action, NextAction]) -> chex.Array:
-        if isinstance(action, Tuple):
+        if not isinstance(dist_params, DistParams):
             assert len(action) == 2
             assert len(dist_params) == 2
             continuous_action_tops =  action[0][1]
@@ -112,7 +123,7 @@ def create_sac_networks(env: AspenDistillation,
     # Lastly let's create
     def sample(dist_params: Union[DistParams, NextDistParams],
                  seed: chex.PRNGKey) -> Union[Action, NextAction]:
-        if isinstance(dist_params, Tuple):
+        if not isinstance(dist_params, DistParams):
             assert len(dist_params) == 2
             key1, key2 = jax.random.split(seed)
             action_tops = sample_single(dist_params[0], key1)
@@ -129,4 +140,3 @@ def create_sac_networks(env: AspenDistillation,
                                sample=sample
                                )
     return sac_networks
-
--- hydrocarbon_problem/agents/sac/create_networks_test.py
+++ hydrocarbon_problem/agents/sac/create_networks_test.py
@@ -6,7 +6,7 @@ from hydrocarbon_problem.env.env import AspenDistillation
 from hydrocarbon_problem.agents.sac.networks import SACNetworks
 
 
-def test_policy_net(env: AspenDistillation, network: SACNetworks):
+def test_critic_net(env: AspenDistillation, network: SACNetworks):
     seed = jax.random.PRNGKey(0)
     obs = env.observation_spec().generate_value()
     action = env.action_spec()[0].generate_value(), env.action_spec()[1].generate_value()
@@ -16,6 +16,25 @@ def test_policy_net(env: AspenDistillation, network: SACNetworks):
     q_value = network.q_network.apply(critic_params, obs, action)
     chex.assert_shape(q_value, (1,))
 
+    next_obs = obs, obs
+    next_action = action, action
+    next_q_value = network.q_network.apply(critic_params, next_obs, next_action)
+    chex.assert_shape(q_value, (1,))
+    assert next_q_value == q_value*2
+
+
+def test_policy_net(env: AspenDistillation, network: SACNetworks):
+    seed = jax.random.PRNGKey(0)
+    obs = env.observation_spec().generate_value()
+
+    policy_params = network.policy_network.init(seed, obs)
+
+    # Now check for single obs
+    dist_params = network.policy_network.apply(policy_params, obs)
+    action = network.sample(dist_params, seed)
+    log_prob = network.log_prob(dist_params, action)
+
+
 
 
 if __name__ == '__main__':
@@ -24,4 +43,5 @@ if __name__ == '__main__':
     env = AspenDistillation(flowsheet_api=FakeDistillationAPI())
     sac_net = create_sac_networks(env=env,
                         policy_hidden_units = (3,), q_value_hidden_units = (10, 10))
+    test_critic_net(env, sac_net)
     test_policy_net(env, sac_net)
